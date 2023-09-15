e7606d91e035b80fcf7e5ed18159cc909d4f7566
Teemu Tanninen <teemu.tanninen@student.lut.fi>
2023-09-15 20:32:46 +0300

    Should pass the 4th test: Editing existing data

--- script.js
+++ script.js
@@ -1,44 +1,106 @@
-document.querySelector('form').addEventListener('submit', (e) => {
-  e.preventDefault();
+let userMatrixArray = [
+  ['Webmaster', 'webmaster@email.com', 'X'],
+  ['User123', 'user123@email.com', '-'],
+  ['AnotherUser123', 'anotheruser123@email.com', '-'],
+];
+// helper function to replace the DOM table with the userMatrixArray
+const replaceHtmlTableWithMatrix = () => {
+  document
+    .querySelector('tbody')
+    .replaceWith(HTMLTableFrom2dArray(userMatrixArray));
+};
+
+// initializer function when window loads
+window.onload = function () {
+  // construct DOM table from the 2D matrix data
+  replaceHtmlTableWithMatrix();
+  // bind submit event function to the form
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitFormData();
+  });
+  // bind empty table function to the button
+  document.querySelector('#empty-table').addEventListener('click', (e) => {
+    emptyTable();
+  });
+};
+
+// helper function to convert Admin checkbox value into 'X' if true or '-' if false
+const adminStringSetter = (isAdmin) => {
+  return isAdmin ? 'X' : '-';
+};
+function submitFormData() {
+  let form = document.querySelector('form');
+  const { username, email, admin } = getFormInputs(form);
+  // if either username or email inputs are empty then don't proceed
+  if (username === '' || email === '') return;
+  let userIndex = indexOfUser(username, userMatrixArray);
+  // if username already exist then change existing values at the table
+  if (userIndex >= 0) {
+    changeExistingUser(userIndex, { username, email, isAdmin: admin });
+    return;
+  }
+  userMatrixArray.push([username, email, adminStringSetter(admin)]);
+  // construct new HTML table from the newly change data matrix array
+  replaceHtmlTableWithMatrix();
+}
+// function to clear form element values
+function clearFormElements() {
   // get form elements from the DOM
   let userInput = document.getElementById('input-username');
   let emailInput = document.getElementById('input-email');
   let adminInput = document.getElementById('input-admin');
-  // if either username or email inputs are empty then don't proceed
-  if (userInput.value === '' || emailInput.value === '') return;
-  // create one table row and three table columns
-  let tableRow = document.createElement('tr');
-  let tableCol1 = document.createElement('td');
-  let tableCol2 = document.createElement('td');
-  let tableCol3 = document.createElement('td');
-  // short function to convert boolean checkbox value into 'X' if true or '-' if false
-  const adminValueSetter = (checkValue) => {
-    return checkValue ? 'X' : '-';
-  };
-  // set table column values to the ones which we get from the form elements
-  tableCol1.textContent = userInput.value;
-  tableCol2.textContent = emailInput.value;
-  tableCol3.textContent = adminValueSetter(adminInput.checked);
-  // add columns to the table row
-  tableRow.appendChild(tableCol1);
-  tableRow.appendChild(tableCol2);
-  tableRow.appendChild(tableCol3);
-  // append new table row to the table body
-  document.querySelector('tbody').append(tableRow);
   // Reset form inputs
   userInput.value = '';
   emailInput.value = '';
   adminInput.checked = false;
-});
-document.querySelector('#empty-table').addEventListener('click', (e) => {
-  emptyTable();
-});
+}
+// helper function to iterate over and return all of the formdata key and values
+function getFormInputs(form) {
+  let formData = new FormData(form);
+  let formKeyValues = {};
+  for (const [key, value] of formData) {
+    formKeyValues[key] = value;
+  }
+  return formKeyValues;
+}
+// function to change existing user at the user data matrix and then replace dom with the new table
+function changeExistingUser(userIndex, { username, email, isAdmin: admin }) {
+  userMatrixArray[userIndex][0] = username;
+  userMatrixArray[userIndex][1] = email;
+  userMatrixArray[userIndex][2] = adminStringSetter(admin);
+  replaceHtmlTableWithMatrix();
+}
 
+// simple function to just empty data matrix and then dom table
 function emptyTable() {
-  document
-    .querySelector('table#table-main')
-    .replaceChild(
-      document.createElement('tbody'),
-      document.querySelector('tbody#table-body')
-    );
+  userMatrixArray = [];
+  replaceHtmlTableWithMatrix();
+}
+
+// function to create DOM represantive table from the 2 dimensinal user data matrix array
+function HTMLTableFrom2dArray(dataArray) {
+  let tableBody = document.createElement('tbody');
+  for (let i = 0; i < dataArray.length; i++) {
+    let dataRow = dataArray[i];
+    let tableRow = document.createElement('tr');
+    for (let j = 0; j < dataRow.length; j++) {
+      let tableCol1 = document.createElement('td');
+      tableCol1.textContent = dataArray[i][j];
+      tableRow.appendChild(tableCol1);
+      tableBody.appendChild(tableRow);
+    }
+  }
+  return tableBody;
+}
+// helper function to find the index of the user inside the 2D data matrix array
+function indexOfUser(user, matrix) {
+  for (var i = 0, len = matrix.length; i < len; i++) {
+    for (var j = 0, len2 = matrix[i].length; j < len2; j++) {
+      if (matrix[i][j] === user) {
+        return i;
+      }
+    }
+  }
+  return -1;
 }
